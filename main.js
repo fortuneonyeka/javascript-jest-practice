b40844cb72edaf7a771bc2c40bd9532f2e275d6a
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-11-02 21:37:19 +0100

    add calculator function

--- main.js
+++ main.js
@@ -15,3 +15,9 @@ export const reverseString = (string) =>{
 //  reverseString("Hello");
 
 
+export const calculator = {
+  add: (x,y) => x + y,
+  multiply: (x,y) => x * y,
+  subtract: (x,y) => x - y,
+  divide: (x,y) => x / y,
+}
