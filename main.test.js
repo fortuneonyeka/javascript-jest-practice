0fad41b8f74db5d6b4d504a34d9ec7b100ac69c6
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-11-02 21:38:00 +0100

    add calculator tests

--- main.test.js
+++ main.test.js
@@ -33,3 +33,54 @@ describe('reverseString', () => {
     expect(reverseString(palindrom)).toBe(palindrom)
   })
 })
+
+describe('calculator', () => {
+  describe('add', () => {
+    test('returns result of adding two number', () => {
+      expect(calculator.add(2,2)).toBe(4)
+    })
+
+    test('add two floats', () => {
+      expect(calculator.add(0.5,0.5)).toBe(1)
+    })
+  })
+  
+  describe('multiplication', () => {
+    test('return the result of multiplying two numbers', () => {
+      expect(calculator.multiply(5,5)).toBe(25)
+    })
+
+    test('multiply two floats', () => {
+      expect(calculator.multiply(2.5,2.5)).toBeCloseTo(6.25)
+    })
+  })
+  describe('diviion', () => {
+    test('return the result of dividing two numbers', () => {
+      expect(calculator.divide(20,5)).toBe(4)
+    })
+
+    test('divide two number', () => {
+      expect(calculator.multiply(10,2)).not.toBe(6.25)
+    })
+    test('divide two number', () => {
+      expect(calculator.multiply(10,2)).toBeGreaterThan(10)
+    })
+    test('divide two number', () => {
+      expect(calculator.multiply(10,2)).toBeLessThanOrEqual(20)
+    })
+  })
+
+  describe('subtract', () => {
+    test('return the result of subtracting a number from another number', () => {
+      expect(calculator.subtract(20,5)).toBe(15)
+    })
+
+    test('subtract two number', () => {
+      expect(calculator.subtract(10,2)).not.toBe(6.25)
+    })
+
+    test('subtract two number', () => {
+      expect(calculator.subtract(10,2)).toBeGreaterThanOrEqual(7)
+    })
+})
+})
